8aac0118b239f3c0c755526823d86f0d1c2ffc9f
suzq1981 <suzq1981@qq.com>
2022-05-13 09:51:36 +0800

    init netty

---
+++ netty-core/src/main/java/com/badou/javaio/bio/SocketMain.java
@@ -0,0 +1,14 @@
+package com.badou.javaio.bio;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SocketMain {
+
+	private static final Logger logger = LoggerFactory.getLogger(SocketMain.class);
+
+	public static void main(String[] args) {
+		logger.info("Start socket server port:{}", 9980);
+	}
+
+}
